e56ba8a8db64bd9a8b4e324bbb878c9d9bf60d02
Joel Lobo <joellobo@gmail.com>
2019-06-03 11:46:49 -0300

    initial commit

---
+++ env.sh
@@ -0,0 +1,6 @@
+
+cd ~
+mkdir curso-introducao-a-ciencia-de-dados
+cd curso-introducao-a-ciencia-de-dados
+wget https://download.knime.org/analytics-platform/linux/knime-latest-linux.gtk.x86_64.tar.gz
+tar -vzxf knime-latest-linux.gtk.x86_64.tar.gz
